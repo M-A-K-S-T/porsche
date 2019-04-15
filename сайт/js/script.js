2a266a01e044c9007838aad158ce85c9529420b9
maksbarsa <43035615+maksbarsa@users.noreply.github.com>
2019-04-15 16:24:09 +0300

    commit1

---
+++ сайт/js/script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+var elems = document.querySelectorAll('.slider');
+var instances = M.Slider.init(elems, {'height' : 800, 'indicators' : false, 'padding' : 20  });
+});
